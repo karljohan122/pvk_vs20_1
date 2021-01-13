1670eb36d323394a45a608b8d32c493b55eb438e
karljohan122 <karljohan122@gmail.com>
2021-01-13 19:56:16 +0200

    Kontroller on määratud ja võetud kasutusele konktrolleri objekti näol

--- app/libs/Core.php
+++ app/libs/Core.php
@@ -3,10 +3,23 @@
 
 class Core
 { //class begin
+    protected $currentController = "Pages";
+    protected $currentMethod = "index";
+    protected $params = [];
     //constructor
     public function __construct()
     {
-        $this->getUrl();
+        $url = $this->getUrl();
+        $controllerName = ucwords($url[0]);
+        $controllerFile = "../app/controllers/".$controllerName.".php";
+        if(file_exists($controllerFile)){
+            $this->currentController = $controllerName;
+            unset($url[0]);
+        }
+        require_once "../app/controllers/".$this->currentController."php";
+        $this->currentController = new $this->currentController;
+        print_r($this->currentController);
+        print_r($url);
     }
 
     //get url data
