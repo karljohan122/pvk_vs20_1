f1f4b8adbe369dc39a985da2690bb1393faf0d4b
karljohan122 <karljohan122@gmail.com>
2021-01-13 19:10:40 +0200

    Loetud lingiga edastatud andmed ja valmistatud ette nende töötluseks abifailid

---
+++ app/libs/Core.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Core
+{ //class begin
+    //constructor
+    public function __construct()
+    {
+        $this->getUrl();
+    }
+
+    //get url data
+    public function getUrl(){
+        if(isset($_GET["url"])){
+            $url = $_GET["url"];
+            echo $url;
+        }
+    }
+} //class end
